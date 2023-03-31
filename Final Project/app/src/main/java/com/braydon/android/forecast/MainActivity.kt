12da9ea0bbdbe4085f6cfad776a8c9c0ed149eaf
Braydon Bell <braydoncbell@gmail.com>
2023-03-31 10:22:31 -0500

    initial setup for all necessary assets

--- Final Project/app/src/main/java/com/braydon/android/forecast/MainActivity.kt
+++ Final Project/app/src/main/java/com/braydon/android/forecast/MainActivity.kt
@@ -1,42 +1,42 @@
-package com.braydon.android.forecast
-
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View
-import android.widget.Button
-
-
-
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var dailyForecastButton: Button
-    private lateinit var hourlyForecastButton: Button
-    private lateinit var historicalWeatherButton: Button
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        dailyForecastButton = findViewById<Button>(R.id.daily_forecast_button)
-        hourlyForecastButton = findViewById<Button>(R.id.hourly_forecast_txt)
-        historicalWeatherButton = findViewById<Button>(R.id.average_forecast_txt)
-
-        dailyForecastButton.setOnClickListener{view: View ->
-            val intent = Intent(this, DailyForecast::class.java)
-            startActivity(intent)
-        }
-
-        hourlyForecastButton.setOnClickListener{view: View ->
-            val intent = Intent(this, HourlyForecast::class.java)
-            startActivity(intent)
-        }
-
-        historicalWeatherButton.setOnClickListener{view: View ->
-            val intent = Intent(this, HistoricalWeather::class.java)
-            startActivity(intent)
-        }
-    }
-
-
-}
+//package com.braydon.android.forecast
+//
+//import android.content.Intent
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+//import android.view.View
+//import android.widget.Button
+//
+//
+//
+//class MainActivity : AppCompatActivity() {
+//
+//    private lateinit var dailyForecastButton: Button
+//    private lateinit var hourlyForecastButton: Button
+//    private lateinit var historicalWeatherButton: Button
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        dailyForecastButton = findViewById<Button>(R.id.daily_forecast_button)
+//        hourlyForecastButton = findViewById<Button>(R.id.hourly_forecast_txt)
+//        historicalWeatherButton = findViewById<Button>(R.id.average_forecast_txt)
+//
+//        dailyForecastButton.setOnClickListener{view: View ->
+//            val intent = Intent(this, DailyForecast::class.java)
+//            startActivity(intent)
+//        }
+//
+//        hourlyForecastButton.setOnClickListener{view: View ->
+//            val intent = Intent(this, HourlyForecast::class.java)
+//            startActivity(intent)
+//        }
+//
+//        historicalWeatherButton.setOnClickListener{view: View ->
+//            val intent = Intent(this, HistoricalWeather::class.java)
+//            startActivity(intent)
+//        }
+//    }
+//
+//
+//}
---
+++ Final Project/app/src/main/java/com/braydon/android/forecast/presentation/ui/theme/Color.kt
@@ -0,0 +1,6 @@
+package com.braydon.android.forecast.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkBlue = Color(0xFF1B3B5A)
+val DeepBlue = Color(0xFF102840)
